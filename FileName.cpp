d01c22e6e6ce164ee866a39a6532dc13c10afc79
ahoyigido <aho.yigido.61@gmail.com>
2024-11-05 09:07:00 +0300

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char operation;
+	float number1, number2;
+
+	cout << "Enter an operation (+, -, *, /): ";
+	cin >> operation;
+
+	cout << "Enter two numbers: ";
+	cin >> number1 >> number2;
+
+	switch (operation)
+	{
+	case '+':
+		cout << number1 << " + " << number2 << " = " << number1 + number2;
+		break;
+	case '-':
+		cout << number1 << " - " << number2 << " = " << number1 - number2;
+		break;
+	case '*':
+		cout << number1 << " * " << number2 << " = " << number1 * number2;
+		break;
+	case '/':
+		cout << number1 << " / " << number2 << " = " << number1 / number2;
+		break;
+	default:
+
+		cout << "ERROR!...";
+		break;
+	}
+
+
+
+	return 0;
+}
